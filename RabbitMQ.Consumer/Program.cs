3dc596cb59f8796a479dae6d0fc6bd7bee513202
sondos emara <sondos.emara2002@gmail.com>
2024-09-12 12:44:46 +0300

    Add project files.

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RabbitMQ.Consumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ RabbitMQ.Producer/Program.cs
@@ -0,0 +1,23 @@
+﻿using RabbitMQ.Client;
+
+namespace RabbitMQ.Producer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost", // Connect to the local RabbitMQ server
+                Port = 5672,            // Default AMQP port
+                UserName = "guest",     // Default username
+                Password = "guest"      // Default password
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                Console.WriteLine("Connected to RabbitMQ server locally!");
+            }
+        }
+    }
+}
